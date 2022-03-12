c49e3ab7e2b08f2be0a701eb93534ea3f8cac53b
priya2022 <glenraise@gmail.com>
2022-03-12 17:49:09 +0530

    up

--- restapi/app.js
+++ restapi/app.js
@@ -37,7 +37,7 @@ app.get('/location',(req,res) => {
         res.send(result)
     })
 })*/
-app.get('/restaurants',(req,res) => {
+app.get('/restaurant',(req,res) => {
     let stateId  = Number(req.query.state_id)
     let mealId = Number(req.query.meal_id)
     let query = {};
